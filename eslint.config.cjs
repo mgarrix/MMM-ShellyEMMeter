b98ce6cc8ff552fb1fb21250d376b854672ca111
mgarrix <maurizio.garritano@gmail.com>
2026-03-26 09:58:40 +0100

    fix

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -21,22 +21,34 @@ module.exports = [
       sourceType: "script",
       globals: {
         // Node
-        require: "readonly",
-        module: "readonly",
-        __dirname: "readonly",
-        console: "readonly",
-        setTimeout: "readonly",
-        setInterval: "readonly",
-        URL: "readonly",
+        require:         "readonly",
+        module:          "readonly",
+        __dirname:       "readonly",
+        console:         "readonly",
+        setTimeout:      "readonly",
+        setInterval:     "readonly",
+        URL:             "readonly",
+        fetch:           "readonly",
+        AbortSignal:     "readonly",
+        AbortController: "readonly",
         // MagicMirror²
-        Module: "readonly",
-        Log: "readonly",
-        document: "readonly",
-        ihtml: "writable"
+        Module:          "readonly",
+        Log:             "readonly",
+        document:        "readonly",
+        ihtml:           "writable"
       }
     },
     rules: {
-
+      "no-unused-vars":    ["warn", { "argsIgnorePattern": "^_" }],
+      "no-var":            "error",
+      "prefer-const":      "warn",
+      "eqeqeq":            ["error", "always"],
+      "no-console":        "off",
+      "semi":              ["error", "always"],
+      "quotes":            ["warn", "double", { "avoidEscape": true }],
+      "object-shorthand":  ["warn", "methods"],
+      "no-trailing-spaces":"warn",
+      "eol-last":          ["warn", "always"]
     }
   }
 ];
